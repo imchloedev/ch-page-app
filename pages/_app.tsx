5f110de31a1d674018fb294ecde1bfb378ca11b0
Eunji Kang <etothej@naver.com>
2023-03-09 00:39:31 +0900

    add: initial setting - env added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-import '@src/styles/globals.css';
 import React from 'react';
 import { Global } from '@emotion/react';
-import globalStyle from '@src/styles/globalStyle';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { RecoilRoot } from 'recoil';
 import type { AppProps } from 'next/app';
+import globalStyle from '@src/styles/globalStyle';
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
