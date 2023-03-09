8da2f989605cd686883e65a083d90845daa95f96
Eunji Kang <etothej@naver.com>
2023-03-09 12:13:47 +0900

    add: SEO component added & set up in Home

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
